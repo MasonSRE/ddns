fd7c2ceda7764cbe771ea6579b7027cca80347e7
MasonYuan <64407414+MasonSRE@users.noreply.github.com>
2023-09-23 15:44:17 +0800

    Update get_ip_dns.py

--- get_ip_dns.py
+++ get_ip_dns.py
@@ -1,10 +1,6 @@
-import time
-import logging
 import requests
-import json
 from aliyunsdkcore.client import AcsClient
-from aliyunsdkalidns.request.v20150109 import UpdateDomainRecordRequest, DescribeDomainRecordsRequest, AddDomainRecordRequest
-from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkalidns.request.v20150109 import UpdateDomainRecordRequest
 
 
 # 阿里云API凭证
@@ -16,33 +12,14 @@ region_id = 'cn-hangzhou'  # 你的阿里云区域ID (可以不做修改)
 domain_name = 'example.com'  # 你的域名
 sub_domain = 'www'  # 子域名，例如www
 
-# 设置日志记录
-logging.basicConfig(level=logging.INFO)
+# 域名和记录信息
+domain_name = 'sreout.com'  # 你的域名
+record_id = '844942334267784192'  # 你的记录ID
+sub_domain = 'me'  # 子域名，例如www
 
 def get_public_ip():
-    try:
-        response = requests.get('https://api.ipify.org?format=json')
-        response.raise_for_status()  # 抛出HTTP错误
-        ip = response.json()['ip']
-        logging.info(f'Public IP retrieved: {ip}')
-        return ip
-    except requests.exceptions.RequestException as e:
-        logging.error(f'Failed to retrieve public IP: {e}')
-        return None
-
-def get_current_dns_record(client, domain_name, sub_domain):
-    request = DescribeDomainRecordsRequest.DescribeDomainRecordsRequest()
-    request.set_DomainName(domain_name)
-    try:
-        response = client.do_action_with_exception(request)
-        records = json.loads(response)['DomainRecords']['Record']
-        for record in records:
-            if record['RR'] == sub_domain and record['Type'] == 'A':
-                return record['RecordId'], record['Value']
-        return None, None
-    except ServerException as e:
-        logging.error(f'Failed to get the current DNS record: {e}')
-        return None, None
+    response = requests.get('https://api.ipify.org?format=json')
+    return response.json()['ip']
 
 def update_dns_record(client, record_id, sub_domain, ip):
     request = UpdateDomainRecordRequest.UpdateDomainRecordRequest()
@@ -50,41 +27,19 @@ def update_dns_record(client, record_id, sub_domain, ip):
     request.set_RR(sub_domain)
     request.set_Type('A')
     request.set_Value(ip)
-    try:
-        client.do_action_with_exception(request)
-        logging.info(f'Updated DNS record to {ip}')
-    except ServerException as e:
-        logging.error(f'Failed to update DNS record: {e}')
+    client.do_action_with_exception(request)
 
-def add_dns_record(client, domain_name, sub_domain, ip):
-    request = AddDomainRecordRequest.AddDomainRecordRequest()
-    request.set_DomainName(domain_name)
-    request.set_RR(sub_domain)
-    request.set_Type('A')
-    request.set_Value(ip)
-    try:
-        client.do_action_with_exception(request)
-        logging.info(f'Added DNS record: {sub_domain}.{domain_name} -> {ip}')
-    except ServerException as e:
-        logging.error(f'Failed to add DNS record: {e}')
-
-def update_dns_record_if_needed(client, domain_name, sub_domain, new_ip):
-    record_id, current_ip = get_current_dns_record(client, domain_name, sub_domain)
-    if record_id is None:
-        logging.warning(f'No existing DNS record found for {sub_domain}.{domain_name}, adding a new record.')
-        add_dns_record(client, domain_name, sub_domain, new_ip)
-        return
-    if current_ip != new_ip:
-        update_dns_record(client, record_id, sub_domain, new_ip)
 
 def main():
     client = AcsClient(access_key_id, access_key_secret, region_id)
-    
+    current_ip = None
+
     while True:
         new_ip = get_public_ip()
-        if new_ip is not None:
-            update_dns_record_if_needed(client, domain_name, sub_domain, new_ip)
-        time.sleep(10)  # 等待10秒再检查
+        if new_ip != current_ip:
+            update_dns_record(client, record_id, sub_domain, new_ip)
+            current_ip = new_ip
+            print(f'Updated DNS record to {new_ip}')
 
 if __name__ == '__main__':
     main()
